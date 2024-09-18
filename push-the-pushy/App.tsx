f3dd128ca8f0ec922392b69ef7de810e42531efb
NodCaleb <nod.caleb@gmail.com>
2024-09-18 14:40:04 +0300

    Pushy package added

--- push-the-pushy/App.tsx
+++ push-the-pushy/App.tsx
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Pushy from 'pushy-react-native';
 
 export default function App() {
+
+  Pushy.listen();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
