02ac4cb0e5154770c60f800641fc23eb72ce94ce
NodCaleb <nod.caleb@gmail.com>
2024-09-20 12:13:57 +0300

    Expo packages for localization and notifications

--- push-the-pushy/App.tsx
+++ push-the-pushy/App.tsx
@@ -6,6 +6,24 @@ export default function App() {
 
   Pushy.listen();
 
+  // Register the user for push notifications
+  Pushy.register()
+      .then(async deviceToken => {
+        // Display an alert with device token
+        alert('Pushy device token: ' + deviceToken);
+
+        // Print token to developer console
+        console.log('Pushy device token: ' + deviceToken);
+
+        // Send the token to your backend server via an HTTP GET request
+        //await fetch('https://your.api.hostname/register/device?token=' + deviceToken);
+        // Succeeded, optionally do something to alert the user
+      })
+      .catch(err => {
+        // Notify user of failure
+        alert('Registration failed: ' + err.message);
+      });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
